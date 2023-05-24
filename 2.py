0fb8cb53b31df089121c92070f646c09997e70be
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-05-24 14:33:52 +0300

    Update 37.py

--- 2.py
+++ 2.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 class AttributePrinterMixin:
     def __str__(self):
         return self._format_attributes(self.__class__, self.__dict__)
@@ -6,22 +9,38 @@ class AttributePrinterMixin:
     def _format_attributes(cls, attributes):
         output = f"{cls.__name__}: {{\n"
         for key, value in attributes.items():
-            if key.startswith("__") and key.endswith("__"):  # Skip built-in attributes
-                continue
-            if key.startswith("_"):
-                if key.startswith(f"_{cls.__name__}__"):  # Private attribute
-                    key = key[len(cls.__name__) + 2:]
-                else:  # Protected attribute
+            classes = [cls] + list(inspect.getmro(cls))[1:-1]
+            for c in classes:
+                name = f"_{c.__name__}__"
+                if key.startswith(name):
+                    key = key[len(name):]
+                    break
+            else:
+                if key.startswith("_"):
                     key = key[1:]
             output += f"\t{key}: {value}\n"
         output += "}"
         return output
-class MyClass(AttributePrinterMixin):
+
+
+
+class A(AttributePrinterMixin):
     def __init__(self):
         self.public_field = 42
         self._protected_field = "protected"
         self.__private_field = [1, 2, 3]
-
-
-obj = MyClass()
+obj = A()
 print(obj)
+
+
+class B:
+    def __init__(self):
+        self.__private_in_B = "private_in_B"
+
+
+class A(AttributePrinterMixin, B):
+    pass
+
+
+a = A()
+print(a)
